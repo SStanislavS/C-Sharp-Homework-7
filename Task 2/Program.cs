3453ee35daac153c7351ed8aece09de58bf4c3fe
Stanislav <S@mail.ru>
2022-12-22 16:52:48 +0300

    add Task 2

---
+++ Task 2/Program.cs
@@ -0,0 +1,39 @@
+﻿// Программа на вход принимает позиции (два индекса) элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+Console.Write("Введите количество строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int [,] numbers = new int [row, column];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+if (row < numbers.GetLength(0) && column < numbers.GetLength(1)) 
+Console.WriteLine(numbers[row, column]);
+else Console.WriteLine($"{row}{column} -> такого числа в массиве нет");
+
+void FillArrayRandomNumbers(int [,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int [,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
