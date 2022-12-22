485432f02572ee7b8ba4ec8562c6e4b765e9f69b
Stanislav <S@mail.ru>
2022-12-22 21:04:55 +0300

    add Task 2

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -1,39 +1,29 @@
 ﻿// Программа на вход принимает позиции (два индекса) элемента в двумерном массиве, 
 // и возвращает значение этого элемента или же указание, что такого элемента нет.
 
-Console.Write("Введите количество строк: ");
+Console.Write("Введите строку: ");
 int row = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите количество столбцов: ");
+Console.Write("Введите столбец: ");
 int column = Convert.ToInt32(Console.ReadLine());
-int [,] numbers = new int [row, column];
-FillArrayRandomNumbers(numbers);
-PrintArray(numbers);
-
-if (row < numbers.GetLength(0) && column < numbers.GetLength(1)) 
-Console.WriteLine(numbers[row, column]);
-else Console.WriteLine($"{row}{column} -> такого числа в массиве нет");
-
-void FillArrayRandomNumbers(int [,] arr)
+int n = 3;
+int m = 4; 
+Random random = new Random();
+int[,] arr = new int[n, m];
+Console.WriteLine("Исходный массив:");
+for (int i = 0; i < arr.GetLength(0); i++)
 {
-    for (int i = 0; i < arr.GetLength(0); i++)
+    for (int j = 0; j < arr.GetLength(1); j++)
     {
-        for (int j = 0; j < arr.GetLength(1); j++)
-        {
-            arr[i, j] = new Random().Next(1, 10);
-        }
+        arr[i, j] = random.Next(1, 10);
+        Console.Write("{0} ", arr[i, j]);
     }
+    Console.WriteLine();
 }
-
-void PrintArray(int [,] arr)
+if (row < 0 | row > arr.GetLength(0) - 1 | row < 0 | column > arr.GetLength(1) - 1)
 {
-    for (int i = 0; i < arr.GetLength(0); i++)
-    {
-        Console.Write("[ ");
-        for (int j = 0; j < arr.GetLength(1); j++)
-        {
-            Console.Write(arr[i, j] + " ");
-        }
-        Console.Write("]");
-        Console.WriteLine("");
-    }
+    Console.WriteLine($"[{row},{column}] -> такого числа в массиве нет");
+}
+else
+{
+    Console.WriteLine("Значение элемента массива = {0}", arr[row, column]);
 }
