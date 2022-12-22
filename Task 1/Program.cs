3e456c00c352bf97ab0db828f5ffdbdd7f335213
Stanislav <S@mail.ru>
2022-12-22 11:20:29 +0300

    add Task 1

---
+++ Task 1/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами. 
+// Тип данных для элементов выбрать double, наличие ненулевой дробной части обязательно.
+
+Console.Write("Введите количество строк m: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов n: ");
+int column = Convert.ToInt32(Console.ReadLine());
+double[,] numbers = new double[row, column];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+}
+
+void PrintArray(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
